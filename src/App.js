f7054754c3495f4e621be1e2455a6845dd4a7819
Ajanaku Temitope Grace <gracetemitope14@gmail.com>
2021-08-11 22:19:37 -0700

    change functional comp to class comp

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
+import React from 'react';
+
 /* eslint-disable */
 import Calculator from './Component/Calculator'; 
 /* eslint-enable */
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-function App() {
-  return (
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  /* eslint-disable */
+  render() {
+    /* eslint-enable */
+    return (
     <div className="App">
-      < Calculator />
-    </div>
-
-  );
+    < Calculator />
+  </div>
+    );
+  }
 }
 
 export default App;
