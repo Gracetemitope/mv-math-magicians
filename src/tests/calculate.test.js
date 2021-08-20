f2f797a13fa6c8133ce61ae50f63ac2538d84675
Carlos Gonzalez <carlosfwrd@gmail.com>
2021-08-20 14:54:23 -0400

    Add test for x operation

--- src/tests/calculate.test.js
+++ src/tests/calculate.test.js
@@ -23,12 +23,21 @@ describe('returns updated calculator data object', () => {
     expect(calculator).toEqual({ total: '2', next: '', operation: ''});
   });
 
-  test('adds 1 - 1 to equal 0', () => {
+  test('substracts 1 - 1 to equal 0', () => {
     let calculator = calculate({}, '1');
     calculator = calculate(calculator, '-');
     calculator = calculate(calculator, '1');
     calculator = calculate(calculator, '=');
 
     expect(calculator).toEqual({ total: '0', next: '', operation: ''});
+  });
+
+  test('multiplies 2 x 2 to equal 4', () => {
+    let calculator = calculate({}, '2');
+    calculator = calculate(calculator, 'x');
+    calculator = calculate(calculator, '2');
+    calculator = calculate(calculator, '=');
+
+    expect(calculator).toEqual({ total: '4', next: '', operation: ''});
   });
 });
