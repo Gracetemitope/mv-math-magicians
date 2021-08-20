9cfce1a79f6b2eeb625e341085d00528577c89df
Carlos Gonzalez <carlosfwrd@gmail.com>
2021-08-20 14:52:43 -0400

    Add test for + operation

--- src/tests/calculate.test.js
+++ src/tests/calculate.test.js
@@ -12,3 +12,14 @@ describe('returns default calculator data object', () => {
     expect(updatedCalculator).toEqual({ total: 0, next: '', operation: ''});
   });
 });
+
+describe('returns updated calculator data object', () => {
+  test('adds 1 + 1 to equal 2', () => {
+    let calculator = calculate({}, '1');
+    calculator = calculate(calculator, '+');
+    calculator = calculate(calculator, '1');
+    calculator = calculate(calculator, '=');
+
+    expect(calculator).toEqual({ total: '2', next: '', operation: ''});
+  });
+});
